af0bdcf015226b2ee7f0ce1365e24b7d5d534217
Harja <rachmadsuharja123@gmail.com>
2024-01-27 23:35:33 +0700

    added preloader

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const navItems = document.querySelectorAll('.navigation-bar .nav-link');
 const home = document.querySelectorAll('#home');
 const aboutme = document.querySelectorAll('#aboutme');
 
+window.addEventListener('load', function() {
+    const preloader = document.getElementById('preloader');
+    preloader.style.display = 'none';
+});
+
 navItems.forEach((navItem) => {
     navItem.addEventListener('click', function() {
         navItems.forEach(function(item) {
