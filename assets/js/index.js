914a84343451340bb57f9ce258096aad7a546671
Rachmad Suharja <rachmadsuharja123@gmail.com>
2024-06-14 07:10:59 +0700

    temp aboutme

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,24 @@
 const tooltipTriggerList = document.querySelectorAll(
-    '[data-bs-toggle="tooltip"]'
+  '[data-bs-toggle="tooltip"]'
 );
 const tooltipList = [...tooltipTriggerList].map(
-    (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
 );
 
-window.addEventListener("load", function() {
-    const preloader = document.getElementById("preloader");
-    preloader.style.display = "none";
+window.addEventListener("load", function () {
+  const preloader = document.getElementById("preloader");
+  preloader.style.display = "none";
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-    // Combination of Formal & Informal
-    // Hi There! I'm Harja. Born in Indonesia, June 4th 2005. Graduated from the Software Engineering major at SMKN 1 Lumajang. I'm very interested in the world of Technology, Astronomy and Geography. My daily activity is to learn new things that I didn't know before. I am very interested in the world of technology, astronomy and geography. I like learning new things that I didn't know before. My hobbies are exercising, reading books and listening to songs, and playing Rubik's cubes.
-    let aboutMe = `Hi there! I'm Harja,
+  // Combination of Formal & Informal
+  // Hi There! I'm Harja. Born in Indonesia, June 4th 2005. Graduated from the Software Engineering major at SMKN 1 Lumajang. I'm very interested in the world of Technology, Astronomy and Geography. My daily activity is to learn new things that I didn't know before. I am very interested in the world of technology, astronomy and geography. I like learning new things that I didn't know before. My hobbies are exercising, reading books and listening to songs, and playing Rubik's cubes.
+  let aboutMe = `Hi there! I'm Harja,
   Born and raised in Indonesia on June 4th, 2005.
   Indonesian Tech Enthusiast fresh out of SMKN 1 Lumajang with a Software Engineering degree (super proud!). 
-  Obsessed with learning new things, especially anything space, geography, or tech-related (gotta conquer this knowledge!). 
+  I'm obsessed with learning new things, especially the mysteries of the universe, knowledge of the world of geography. or dives deep into the ever-evolving world of technology. Also I enjoy reading books and listening to music.
   When I'm not hitting the books or jamming to tunes, you might find me working out or tackling a Rubik's Cube.
+  So, that's me in a nutshell. What about you? Let's connect and see where our journeys take us!
   `;
-    document.getElementById("aboutme-content").innerText = aboutMe;
+  document.getElementById("aboutme-content").innerText = aboutMe;
 });
--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,24 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const navbar = document.querySelector("ul");
-    const links = navbar.getElementsByTagName("a");
+  const navbar = document.querySelector("ul");
+  const links = navbar.getElementsByTagName("a");
 
-    window.addEventListener("scroll", () => {
-        const currentScroll = document.documentElement.scrollTop;
-        for (let i = 0; i < links.length; i++) {
-            let link = links[i];
-            let sectionId = link.getAttribute("href");
-            let section = document.querySelector(sectionId);
-            if (
-                section &&
-                section.offsetTop <= currentScroll &&
-                section.offsetTop + section.offsetHeight > currentScroll
-            ) {
-                link.style.fontWeight = 650;
-                link.style.color = "#00abf0";
-            } else {
-                link.style.fontWeight = "normal";
-                link.style.color = "#ededed";
-            }
-        }
-    });
+  window.addEventListener("scroll", () => {
+    const currentScroll = document.documentElement.scrollTop;
+    for (let i = 0; i < links.length; i++) {
+      let link = links[i];
+      let sectionId = link.getAttribute("href");
+      let section = document.querySelector(sectionId);
+      if (
+        section &&
+        section.offsetTop <= currentScroll &&
+        section.offsetTop + section.offsetHeight > currentScroll
+      ) {
+        link.style.fontWeight = 650;
+        link.style.color = "var(--main-color)";
+      } else {
+        link.style.fontWeight = "normal";
+        link.style.color = "#ededed";
+      }
+    }
+  });
 });
